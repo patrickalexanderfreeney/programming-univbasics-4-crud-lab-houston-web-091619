ca63a72c556385bc8d9c6ce6bede9d47a2de479f
Patrick Alexander <patrick_alexander@mail.com>
2019-08-28 19:41:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
   my_names = ["patrick", "alexander", "freeney", "Junior"]
 end
 
-def add_element_to_end_of_array(array, element)s
+def add_element_to_end_of_array(array, element)
   
 end
 
